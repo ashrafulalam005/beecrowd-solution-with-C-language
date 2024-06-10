0c69aa0ec96f97a48eb75ed2182ec71297a08e46
Ashraful Alam <ashrafulalamyt@gmail.com>
2024-06-11 00:49:10 +0600

    Update 1009.c

---
+++ beginner/1009.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char name;
+    double salary, value, total;
+    scanf("%s %lf %lf", &name, &salary, &value);
+    total = salary + (value*.15);
+    printf("TOTAL = R$ %.2lf\n", total);
+    return 0;
+}
