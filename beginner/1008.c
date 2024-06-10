0e3531952c324592d55de50288fa103066a17a52
Ashraful Alam <ashrafulalamyt@gmail.com>
2024-06-11 00:36:55 +0600

    Update 1008.c

---
+++ beginner/1008.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a, b;
+    double c, SALARY;
+    scanf("%d %d %lf", &a, &b, &c);
+    SALARY = (b*c);
+    printf("NUMBER = %d\n", a);
+    printf("SALARY = U$ %.2lf\n", SALARY);
+    return 0;
+}
