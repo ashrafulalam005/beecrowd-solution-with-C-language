dc0b6dbb6f2066c48df1a8ea108000c84877a5d9
Ashraful Alam <ashrafulalamyt@gmail.com>
2024-07-09 09:13:07 +0600

    Update 1015.c

---
+++ beginner/1015.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    double x1, x2, y1, y2, distance;
+    scanf("%lf %lf", &x1, &y1);
+    scanf("%lf %lf", &x2, &y2);
+
+    distance = sqrt(pow(x2-x1,2)+pow(y2-y1,2));
+
+    printf("%.4lf\n", distance);
+    return 0;
+}
